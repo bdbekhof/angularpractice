7bb8d3e71ef3727701b4b43c06dbe63c9aeab85c
Brian Bekhof <brian.bekhof@nccw.nl>
2025-07-24 10:24:20 +0200

    Changed to SPA using routeroutlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FirstComponentComponent } from "./component/first-component/first-component.component";
-import { IntroductionComponent } from "./component/introduction/introduction.component";
-import { ColorChangeComponent } from "./component/color-change/color-change.component";
-import { StudentListComponent } from "./component/student-list/student-list.component";
+import { NavbarComponent } from "./component/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponentComponent, IntroductionComponent, ColorChangeComponent, StudentListComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FirstComponentComponent } from './component/first-component/first-component.component';
+import { IntroductionComponent } from './component/introduction/introduction.component';
+import { ColorChangeComponent } from './component/color-change/color-change.component';
+import { StudentListComponent } from './component/student-list/student-list.component';
+import { ContactFormComponent } from './component/contact-form/contact-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FirstComponentComponent },
+    { path: 'introduction', component: IntroductionComponent },
+    { path: 'color-change', component: ColorChangeComponent },
+    { path: 'student-list', component: StudentListComponent },
+    { path: 'contact-form', component: ContactFormComponent},
+];
